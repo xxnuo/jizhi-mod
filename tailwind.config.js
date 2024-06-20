8efe3bcc51f16d413765be785e7882e1034cecab
xxnuo <bigtear@vip.qq.com>
2024-06-20 10:59:01 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { DARK_THEME, LIGHT_THEME } from "./components/Constants";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./App.jsx", "./Layout.jsx", "./components/*.js", "./entrypoints/newtab/index.html"],
   plugins: [require("daisyui")],
   daisyui: {
     themes: [LIGHT_THEME, DARK_THEME],
