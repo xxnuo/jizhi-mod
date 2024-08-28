f4b0ced4c169da7280b51222784eb0d2a9fa7e10
Kerry <54252779+xxnuo@users.noreply.github.com>
2024-08-28 09:10:40 +0800

    添加京华老宋体

--- components/Constants.js
+++ components/Constants.js
@@ -2,6 +2,14 @@ export const LIGHT_THEME = "cupcake";
 // export const DARK_THEME = "luxury"
 export const DARK_THEME = "halloween";
 
-export const FONTNAME_LIST = ["nzgrKangxi", "QIJIFALLBACK", "Slidefu", "YuFanXinYu", "MuzaiPixel", "LXGW WenKai Light"];
+export const FONTNAME_LIST = [
+  "nzgrKangxi",
+  "QIJIFALLBACK",
+  "Slidefu",
+  "YuFanXinYu",
+  "MuzaiPixel",
+  "LXGW WenKai Light",
+  "KingHwa_OldSong",
+];
 
 export const POEM_MAXLINELENGTH = 23;
