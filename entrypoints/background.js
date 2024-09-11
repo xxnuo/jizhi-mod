43b84361b5c5a4bf79153718fd50ce84c4f75110
Kerry <54252779+xxnuo@users.noreply.github.com>
2024-09-11 14:38:57 +0800

    Update background.js

--- entrypoints/background.js
+++ entrypoints/background.js
@@ -7,7 +7,7 @@ export default defineBackground(() => {
   });
 
   browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getVoice") {
+    if (request.action === "getVoice" && request.text) {
       getVoice(request.text)
         .then((response) => {
           if (response.ok) {
@@ -17,15 +17,16 @@ export default defineBackground(() => {
           }
         })
         .then((blob) => {
-          // console.log("Got voice:", blob);
-          console.log("Got voice");
+          // console.log("Got voice", blob.size);
           const reader = new FileReader();
           reader.onloadend = () => {
             const dataUrl = reader.result;
             sendResponse({ url: dataUrl });
           };
           reader.onerror = () => {
-            throw new Error("Failed to read blob as data URL");
+            console.error("Failed to read blob as data URL");
+            sendResponse({ error: "Failed to read blob as data URL" });
+            reader = null; // 清理 reader 对象
           };
           reader.readAsDataURL(blob);
         })
