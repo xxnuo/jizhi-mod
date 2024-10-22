9aa96fb9327d47c40530081b653b9b950c74ccf1
Kerry <54252779+xxnuo@users.noreply.github.com>
2024-10-22 22:51:39 +0800

    opt

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ import { DARK_THEME, LIGHT_THEME } from "./pages/components/constants";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./pages/**/*.{js,ts,jsx,tsx,mdx}", "./entrypoints/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
+  content: ["./index.html", "./pages/**/*.{js,jsx}", "./entrypoints/**/*.{js,jsx}"],
   plugins: [require("daisyui")],
   daisyui: {
     themes: [LIGHT_THEME, DARK_THEME],
--- wxt.config.js
+++ wxt.config.js
@@ -3,10 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
-  vite: () => ({
-    // plugins: [
-    // ],
-  }),
+  vite: () => ({}),
   manifest: {
     author: "xxnuo",
     name: "浮生梦",
